50a11cb1b6d1b7dd7d5d20255d869b8e7e7d9ecd
Anuj <anuj.middha@gmail.com>
2016-02-12 15:31:12 +0530

    wait for emulator to boot before running tests

--- test.sh
+++ test.sh
@@ -1,4 +1,17 @@
-emulator -avd lollipop -no-skin -no-audio -no-window &
+emulator -avd Nexus_5X_API_23 &
+
+echo "waiting for device"
 adb wait-for-device
+
+echo "wait over, now waiting for boot"
+A=$(adb shell getprop sys.boot_completed | tr -d '\r')
+
+while [ "$A" != "1" ]; do
+        sleep 2
+        A=$(adb shell getprop sys.boot_completed | tr -d '\r')
+done
+
+echo "device boot completed"
 adb shell input keyevent 82 &
-./gradlew connectedAndroidTest
+
+./gradlew connectedCheck
---
+++ wait_for_emulator.sh
@@ -0,0 +1,6 @@
+A=$(adb shell getprop sys.boot_completed | tr -d '\r')
+
+while [ "$A" != "1" ]; do
+        sleep 2
+        A=$(adb shell getprop sys.boot_completed | tr -d '\r')
+done
